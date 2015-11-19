913bd3ac35fbd81ac0a033bb4a6b7354f1c22366
vikramuk <vikramuk@gmail.com>
2015-11-19 11:35:50 +0530

    Update Latest.py

--- Py-Serial/Latest.py
+++ Py-Serial/Latest.py
@@ -1,6 +1,5 @@
 import time
 import sys
-import time
 import usb
 import serial
 import visa
@@ -9,13 +8,13 @@ import visa
 AGILENT_33220A= "USB0::0x0957::0x0407::MY44021621::0::INSTR" # - for 33220A
 AGILENT_33522A= "USB0::0x0957::0x2307::MY50003961::0::INSTR" # - for 33522A
 COMPORT =5
-COMPORT=1 if (COMPORT ==0) else 5   # Check for COMPORT to other than Zero
+COMPORT=2 if (COMPORT ==0) else 5   # Check for COMPORT to other than Zero
 BAUDRATE= 115200
 
 def setUpCOMPort():
     try:
         ser = serial.Serial((COMPORT-1),BAUDRATE, parity='N',bytesize=8, stopbits=1, timeout=None, xonxoff=False,rtscts=False, dsrdtr=False)  
-        print ser.name      # check whi ch port was really used
+        print ser.name      # check which port was really used
         ser.write("US 1 1 130\n")
         print ("Sending Values to the US1 Channel")      
         time.sleep(5)
@@ -25,9 +24,9 @@ def setUpCOMPort():
         print("Disconnecting")
     #Fail Gracefully
     except IOError:
-        print 'cannot Connect to Device: '+ "COM4"        
+        print 'cannot Connect to Device: '+ str(float(COMPORT-1))+'PORT'        
     except Exception as e:
-        print 'cannot Find the Device: '+ "COM4"
+        print 'cannot Find the Device: '+ str(float(COMPORT-1))+'PORT'  
         ser.close()
     else:
         print "COM Port:" + str(COMPORT)+ " Connection has been Closed"
@@ -106,17 +105,20 @@ def set_Values_USB():
         print "Connection has been Closed"       
 
 def launch_APK_AUT():
+    start.application("com.ge.med.mic.lotus.squish")
     '''
-    #start.application("com.ge.med.mic.lotus.squish")
     ctx0 = startApplication("com.ge.med.mic.lotus.squish")
     setApplicationContext(ctx0)
     #"com.ge.med.mic.lotus.squish"  "com.ge.med.mic.lotus"
-    '''        
+    '''
     waitForObject(":Monitoring Activity.Chart_Button")
-    waitForObject("Monitoring Activity_Activity.fhr_Text")
-    test.compare(findObject(":Monitoring Activity_Activity'").text, 100,"Checking values displayed on the Chart")
-    test.compare(waitForObject("Monitoring Activity_Activity.fhr_Text").text, 139)
-    test.verify("AUT Values", "AUT Values")    
+    fhr = waitForObject(":Monitoring Activity.fhr_Text")
+    test.verify(fhr,"FHR is Present")
+    fhrVal = waitForObject(":Monitoring Activity.fhr_Text").text   #  fhr.text
+    for i in range(0,4):
+        test.verify(fhrVal > 49 and fhrVal < 51, "FHR value is between 49 and 51")        
+    test.compare(fhrVal,"49") #waitForObject(":Monitoring Activity.fhr_Text").text, "139")        
+    test.verify(fhrVal > 49 and fhrVal < 51, "FHR value is between 49 and 51")       
     '''    
     {container=':Monitoring Activity_Activity' resourceName='fhr' text='Chart' type='Text' visible='true'}
     {container=':Monitoring Activity_Activity' resourceName='lib_navigation_chart_button' text='Chart' type='Button' visible='true'}
@@ -133,14 +135,18 @@ def launch_APK_AUT():
     test.verify("AUT", "AUT")
     pass
 
-def Set_Burst_Period():
+def Set_Burst_Period(period):
 # Check for Device Availability
     try:
         rm = visa.ResourceManager()
         rm.list_resources()
         inst_33220A = rm.open_resource(AGILENT_33220A, read_termination="\r")
         inst_33220A.timeout = 25  
-        print(inst_33220A.write(":SOUR1:BURS:INT:PER 0.5 S"))
+        period =0.1 if (period == 0) else float(period)
+        #(":SOUR1:BURS:INT:PER 0.5 S"))
+        print(inst_33220A.write(":SOUR1:BURS:INT:PER"+ str(float(period))+ "S ")
+        burst_period = float(inst_33220A.query(":SOUR:BURS:INT:PER?"))
+        print "Burst Period:    ", burst_period # " seconds"
     #Fail Gracefully
     except IOError:
         print 'cannot Connect to Device: '+ AGILENT_33220A
@@ -155,18 +161,22 @@ def main():
     #Set Values
     set_Values_USB()
     #Re-read the set values 
-    test_Values_USB()    
+    test_Values_USB()
+    #change the Burst period 
+    Set_Burst_Period(0.1)
     #Launch the APK and connect to AUT
     launch_APK_AUT()
     #change the Burst period 
-    Set_Burst_Period()    
+    Set_Burst_Period(0.5)    
     #Get Initial Values
     test_Values_USB() 
     #Sleep for Period
-    time.sleep(3)
-    #Send Commands on COM Port
-    #setUpCOMPort()
+    time.sleep(3)    
+    if (checkEnabled==1):     #Send Commands on COM Port    
+        setUpCOMPort()    
     
 
 if __name__ == '__main__':
+    checkEnabled =0
     main()
+    
